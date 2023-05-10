d544851b3e19daa01c3c6621e919d002f58494c4
Shubham <shubham@softobiz.com>
2023-05-10 12:39:27 +0530

    product listing page empty state & create product component added

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListingComponent } from './product-listing/product-listing.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 const routes: Routes = [
   {
     path:'',
     component: ProductListingComponent
+  },
+  {
+    path:'create-product',
+    component: CreateProductComponent
   }
 ];
 
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListingComponent } from './product-listing/product-listing.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [
-    ProductListingComponent
+    CreateProductComponent,
+    ProductListingComponent,
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class ProductsModule { }
