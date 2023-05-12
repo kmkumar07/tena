052f358f9607596b2190fdbdcc038d1004ee2222
Shubham <shubham@softobiz.com>
2023-05-12 13:00:04 +0530

    added the dialog box in the create product page

--- src/app/modules/products/create-product/create-product.component.ts
+++ src/app/modules/products/create-product/create-product.component.ts
@@ -1,9 +1,57 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { NgxTippyProps, NgxTippyService } from 'ngx-tippy-wrapper';
+
 
 @Component({
   selector: 'app-create-product',
   templateUrl: './create-product.component.html',
-  styleUrls: ['./create-product.component.scss']
+  styleUrls: ['./create-product.component.scss'],
 })
 export class CreateProductComponent {
+  @ViewChild('tippyTemplate', { read: ElementRef, static: true })
+  tippyTemplate: ElementRef;
+  tippyContent: NgxTippyProps = {};
+  constructor(
+    public dialog: MatDialog,
+    private ngxTippyService: NgxTippyService
+  ) {}
+  // ngAfterViewInit() {
+  //   this.setContentForTooltip();
+  // }
+  openDialog(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ): void {
+    this.dialog.open(DialogAnimationsDialog, {
+      width: '',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+  // setContentForTooltip() {
+  //   const template = this.tippyTemplate.nativeElement;
+
+  //   // Pass element itself
+  //   this.ngxTippyService.setContent('content', template);
+
+  //   // or
+
+  //   // Pass element `innerHTML`
+  //   this.ngxTippyService.setContent('content', template.innerHTML);
+  // }
+}
+
+@Component({
+  selector: 'dialog-animations-dialog',
+  templateUrl: '../../../shared/components/dialog-box/dialog-animations-dialog.html',
+  styleUrls: ['../../../shared/components/dialog-box/dialog-animations.scss'],
+})
+export class DialogAnimationsDialog {
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsDialog>) {}
 }
--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -4,20 +4,21 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListingComponent } from './product-listing/product-listing.component';
 import { CreateProductComponent } from './create-product/create-product.component';
+import { DialogAnimationsDialog } from './create-product/create-product.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 import { AngularMaterialModule } from 'src/app/shared/modules/angular-material/angular-material.module';
 import { NgxTippyModule } from 'ngx-tippy-wrapper';
 @NgModule({
-  declarations: [CreateProductComponent, ProductListingComponent],
+  declarations: [CreateProductComponent, ProductListingComponent, DialogAnimationsDialog],
   imports: [
     CommonModule,
     ProductsRoutingModule,
     MatButtonModule,
     MatIconModule,
     AngularMaterialModule,
-    NgxTippyModule
+    NgxTippyModule,
   ],
 })
 export class ProductsModule {}
