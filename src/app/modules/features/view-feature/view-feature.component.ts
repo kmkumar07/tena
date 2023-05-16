a37b301f64f7d52fc129f5d61e9c8744d18c5912
Shubham <shubham@softobiz.com>
2023-05-16 18:35:26 +0530

    added html of view feature page

--- src/app/modules/features/view-feature/view-feature.component.ts
+++ src/app/modules/features/view-feature/view-feature.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  weight: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { name: 'Hydrogen', weight: 1.0079 },
+  { name: 'Helium', weight: 4.0026 },
+];
+
 @Component({
   selector: 'app-view-feature',
   templateUrl: './view-feature.component.html',
-  styleUrls: ['./view-feature.component.scss']
+  styleUrls: ['./view-feature.component.scss'],
 })
 export class ViewFeatureComponent {
-
+  displayedColumns: string[] = ['name', 'weight'];
+  dataSource = ELEMENT_DATA;
+  clickedRows = new Set<PeriodicElement>();
 }
