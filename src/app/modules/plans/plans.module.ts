9acbc9a2a778c00a24009e44fb3e343bec1fccab
phurbu choedon <phurbu.choedon@softobiz.com>
2023-05-22 11:30:13 +0530

    side-stepper component

--- src/app/modules/plans/plans.module.ts
+++ src/app/modules/plans/plans.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PlansRoutingModule } from './plans-routing.module';
+import { SideStepperComponent } from './side-stepper/side-stepper.component';
+
+import { AngularMaterialModule } from 'src/app/shared/modules/angular-material/angular-material.module';
+import { SharedModule } from 'src/app/shared/modules/shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideStepperComponent
+  ],
   imports: [
     CommonModule,
-    PlansRoutingModule
+    PlansRoutingModule,
+    AngularMaterialModule,
+    SharedModule
+
   ]
 })
 export class PlansModule { }
