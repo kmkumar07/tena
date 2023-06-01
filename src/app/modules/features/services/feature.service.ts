da42663e921619abc4d40e0feae33027d38bec5e
Akhand Pratap <akhandpratap121196@gmail.com>
2023-06-01 18:21:13 +0530

    feature list and delete integrate in front-end

--- src/app/modules/features/services/feature.service.ts
+++ src/app/modules/features/services/feature.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, map, Observable, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -22,6 +22,31 @@ export class FeatureService {
       catchError((err) => {
         this.error$.next(err.message);
         throw err;
+      })
+    );
+  }
+  
+  getFeatures(PageNumber: number, limit: number): Observable<any[]> {
+    return this.http
+      .get<any>(`${environment.featureUrl}?page=${PageNumber}&limit=${limit}`)
+      .pipe(
+        map((res) => {
+          this.featureSubject.next(res);          
+          return res.data;
+        }),
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      );
+  }
+  deleteFeature(id: any): Observable<any> {
+    const url = `${environment.featureUrl}/{featureId}?featureId=${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.error$.next(error.message);
+        throw error;
       })
     );
   }
