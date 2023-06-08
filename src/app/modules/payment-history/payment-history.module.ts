c722813bd5e9f6c0d12ebc05356fe052ff07263e
Namit-Jaitly <Namit.Jaitly@softobiz.com>
2023-06-08 12:46:40 +0530

    payment history module and routing for components inside it

---
+++ src/app/modules/payment-history/payment-history.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PaymentHistoryRoutingModule } from './payment-history-routing.module';
+import { TransactionListComponent } from './transactions/components/transaction-list/transaction-list.component';
+import { TransactionViewComponent } from './transactions/components/transaction-view/transaction-view.component';
+import { LogsListComponent } from './logs/components/logs-list/logs-list.component';
+import { LogViewComponent } from './logs/components/log-view/log-view.component';
+import { InvoiceListComponent } from './invoices/components/invoice-list/invoice-list.component';
+import { InvoiceViewComponent } from './invoices/components/invoice-view/invoice-view.component';
+
+
+@NgModule({
+  declarations: [
+    TransactionListComponent,
+    TransactionViewComponent,
+    LogsListComponent,
+    LogViewComponent,
+    InvoiceListComponent,
+    InvoiceViewComponent
+  ],
+  imports: [
+    CommonModule,
+    PaymentHistoryRoutingModule
+  ]
+})
+export class PaymentHistoryModule { }
