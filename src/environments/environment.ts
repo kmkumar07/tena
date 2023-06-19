f1314e73964551226b23d238359c2490639b10cb
MUTHUKUMAR K <MUTHUKUMAR.K@softobiz.com>
2023-06-19 15:16:17 +0000

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,  
-  uploadUrl:'http://[::1]:4201/pcm/api/v1/blob/upload',
-  apiUrl: 'http://[::1]:4201/pcm/api/v1',
+  production: true,  
+  uploadUrl:'https://dev-kong-saasframework.symple.co.in/pcm/api/v1/blob/upload',
+  apiUrl: 'https://dev-kong-saasframework.symple.co.in/pcm/api/v1',
   blobStorageUrl:'https://saasframeworkuploads.blob.core.windows.net',
   kratosUrl:'http://127.0.0.1:4433/self-service',
 
