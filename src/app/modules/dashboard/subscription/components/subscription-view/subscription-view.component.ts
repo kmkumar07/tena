21679b22f102df8a00def98e403fdb4f50511aef
phurbu choedon <phurbu.choedon@softobiz.com>
2023-06-20 15:47:16 +0530

    Html design for overview of Subscription  listing/148393

--- src/app/modules/dashboard/subscription/components/subscription-view/subscription-view.component.ts
+++ src/app/modules/dashboard/subscription/components/subscription-view/subscription-view.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { EditCustomerDetailsComponent } from 'src/app/shared/components/dialog-box/edit-customer-details/edit-customer-details.component';
 
 @Component({
   selector: 'app-subscription-view',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./subscription-view.component.scss']
 })
 export class SubscriptionViewComponent {
-
+  dialogRef: any;
+  invoices: any[] = [{
+    'id': 'SO - 001',
+    'occurOn': 'Jan 09, 2023',
+    'status': 'Paid',
+    'paymentMethod': '2341',
+    'amount': '899.00'
+  }]
+  activityLogs: any[] = [{
+    'timeStamp': 'Aug 7th, 2022 9:09am',
+    'events': 'Subscription has been created for the Plan Premium USD - Monthly.',
+    'customerInfo': 'Greenplus Enterprises',
+    'eventSource': 'Via Portal'
+  }]
+  invoiceCols: string[] = [
+    'id',
+    'occurOn',
+    'status',
+    'paymentMethod',
+    'amount'
+  ]
+  logsCols: string[] = [
+    'timeStamp',
+    'events',
+    'customerInfo',
+    'eventSource'
+  ]
+  constructor(public dialog: MatDialog) {}
+  openEdit(type: string) {
+    this.dialogRef = this.dialog.open(EditCustomerDetailsComponent, {
+      width: '29rem',
+      panelClass: 'edit-customer',
+      data: {
+        type: type,
+      },
+    });
+  }
 }
