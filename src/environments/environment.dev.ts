6948f6266e8b3044b36a693313ac8e651ec07a9b
Akash Kumar <Akash.Kumar@softobiz.com>
2023-06-29 09:20:38 +0530

    custom quantity and range added in product-variant

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,10 +1,7 @@
 export const environment = {
   production: false,
-  uploadUrl:
-    'https://dev-kong-saasframework.symple.co.in/pcm/api/v1/blob/upload',
- // apiUrl: 'https://dev-kong-saasframework.symple.co.in/pcm/api/v1',
+  uploadUrl:'https://dev-kong-saasframework.symple.co.in/pcm/api/v1',
+  apiUrl: 'https://dev-kong-saasframework.symple.co.in/pcm/api/v1',
   blobStorageUrl: 'https://saasframeworkuploads.blob.core.windows.net',
   kratosUrl: 'http://127.0.0.1:4433/self-service',
-  apiUrl:'http://[::1]:4201/nest-boilerplate/api/v1'
-
 };
