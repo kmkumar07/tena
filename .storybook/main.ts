b9c0eebb2b4b2a5915e95f6cc2481f3575182358
Namit-Jaitly <Namit.Jaitly@softobiz.com>
2023-07-10 17:54:33 +0530

    themeing updated

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+
 import type { StorybookConfig } from "@storybook/angular";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
