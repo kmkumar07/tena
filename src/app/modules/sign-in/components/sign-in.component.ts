66427ad641d5bf2a427c2a430728b2dc85a34e73
Vishal Raj <vishal.raj@softobiz.com>
2023-07-13 16:31:59 +0530

    patch fixed

--- src/app/modules/sign-in/components/sign-in.component.ts
+++ src/app/modules/sign-in/components/sign-in.component.ts
@@ -49,9 +49,9 @@ export class SignInComponent implements OnInit {
       },
       error: (err: any) => {
         this.loading = false
-        if (err?.status === 400 ) {
-          this.errorMessage = "The provided credentials are invalid, check for spelling mistakes in your password or username, email address."
-        } else if (err?.status === 500) {
+        if (err?.status === 400 || err?.error.ui.messages[0]?.id === 4000006) {
+          this.errorMessage = err?.error.ui.messages[0]?.text
+       } else if (err?.status === 500) {
           this.errorMessage = "An internal server error occurred. Please try again later.";
         } else {
           this.errorMessage = "An error occurred. Please try again later.";
