a51e5a5b4740e437a442cc8baea1115c0a037e07
Namit-Jaitly <Namit.Jaitly@softobiz.com>
2023-07-19 13:28:19 +0530

    accidentally committed env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,  
-  // apiUrl: 'https://dev-kong-saasframework.symple.co.in/pcm/api/v1',
-  apiUrl: 'http://[::1]:4201/nest-boilerplate/api/v1',
+  apiUrl: 'https://dev-kong-saasframework.symple.co.in/pcm/api/v1',
   blobStorage:'https://saasframeworkuploads.blob.core.windows.net',
   kratosUrl:'http://127.0.0.1:4433/self-service',
 
