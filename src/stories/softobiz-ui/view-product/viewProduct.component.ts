1aa3756662ce07bf9c5b4270672ad1103113e3ec
phurbu choedon <phurbu.choedon@softobiz.com>
2023-07-27 16:30:56 +0530

    Task 152049Update the Create Feature Page design in "View Product " Page

---
+++ src/stories/softobiz-ui/view-product/viewProduct.component.ts
@@ -0,0 +1,10 @@
+import { Component , Input } from '@angular/core';
+
+@Component({
+  selector: 'sft-viewProduct',
+  templateUrl: './viewProduct.component.html',
+  styleUrls: ['./viewProduct.component.scss']
+})
+export class viewProductComponent {
+
+}
