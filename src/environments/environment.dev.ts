ddce6660f01d5661703d44e2d7d682408af4c57c
Gopi Prasanth <Gopi.Prasanth@softobiz.com>
2023-07-28 12:42:21 +0000

    Updated environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-   apiUrl: 'https://dev-kong-saasframework.symple.co.in',
+   apiUrl: 'https://dev-kong-saasframework.symple.co.in/pcm/api/v1',
   blobStorage: 'https://saasframeworkuploads.blob.core.windows.net',
   kratosUrl: 'https://dev-kong-saasframework.symple.co.in/.ory/kratos/public/self-service',
 };
