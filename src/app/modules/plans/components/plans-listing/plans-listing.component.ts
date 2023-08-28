4582931b9dc8e4f563c3ce7f3c5f6a8f6ba0b524
Abin Dhiman <Abin.Dhiman@softobiz.com>
2023-08-28 19:04:33 +0530

    sorting and searching implemented in plans

--- src/app/modules/plans/components/plans-listing/plans-listing.component.ts
+++ src/app/modules/plans/components/plans-listing/plans-listing.component.ts
@@ -1,30 +1,49 @@
-import { Component, ViewChild } from '@angular/core';
-import { noPlans, plansFields } from 'src/app/shared/constants/consants';
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { SelectionModel } from '@angular/cdk/collections';
+import { Component, ViewChild, OnDestroy } from '@angular/core';
+import {
+  Subject,
+  Subscription,
+  debounceTime,
+  distinctUntilChanged,
+  takeUntil,
+} from 'rxjs';
 import { MatSort, Sort } from '@angular/material/sort';
-import { PlanService } from '../../services/plan.service';
-import { takeUntil } from 'rxjs';
-import { GlobalService } from 'src/app/core/services/global.service';
 import { MatDialog } from '@angular/material/dialog';
-import { DeleteConfirmationComponent } from 'src/app/shared/components/dialog-box/delete-confirmation/delete-confirmation.component';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { SelectionModel } from '@angular/cdk/collections';
+import { PlanService } from '../../services/plan.service';
+import { GlobalService } from 'src/app/core/services/global.service';
+import { DeleteConfirmationComponent } from 'src/app/shared/components/dialog-box/delete-confirmation/delete-confirmation.component';
 import { CouponsDeleteSuccessComponent } from 'src/app/shared/components/dialog-box/coupons-delete-success/coupons-delete-success.component';
+import { noPlans, plansFields } from 'src/app/shared/constants/consants';
+
 @Component({
   selector: 'app-plans-listing',
   templateUrl: './plans-listing.component.html',
   styleUrls: ['./plans-listing.component.scss'],
 })
-export class PlansListingComponent {
+export class PlansListingComponent implements OnDestroy {
+  plansSearchData: any;
   emptyList = noPlans;
-  plansData: Array<any>;
+  plansData: Array<any> = [];
   planAllData: any;
-  plan: Array<any>;
+  totalNumberOfPlan: number;
+  plan: Array<any> = [];
   planLength: number;
+  totalPages: number = 0;
+  hasNextPage: boolean = false;
+  totalNumberOfPlanBySearch: number;
+  plansearchDataNextPage: boolean = false;
   pageNumber: number = 1;
+  NumberOfPage: any = '';
+  NumberOfLimit: any = '';
   limit: number = 5;
   search: string = '';
+  searchQuery: string = '';
+  sortBy: 'externalName' | 'createdOn';
+  sortOrder: 'asc' | 'desc';
+  allPlansData: number = 0;
   displayedColumns: string[] = [
     'plan_ID',
     'external_name',
@@ -33,6 +52,9 @@ export class PlansListingComponent {
     'status',
     'action',
   ];
+  private searchQueryChanged: Subject<string> = new Subject<string>();
+  private searchSubscription: Subscription;
+
   selection = new SelectionModel<plansFields>(true, []);
   @ViewChild(MatSort) sort: MatSort;
   dialogRef: any;
@@ -46,42 +68,99 @@ export class PlansListingComponent {
     private snackBar: MatSnackBar
   ) {}
 
-  ngOnInit() {
-    setTimeout(() => {
-      this.getPlans(this.pageNumber, this.limit, this.search);
-    });
+  ngOnInit(): void {
+    this.sortBy = 'createdOn';
+    this.sortOrder = 'desc';
+    this.getPlans(
+      this.pageNumber,
+      this.limit,
+      this.search,
+      this.sortBy,
+      this.sortOrder
+    );
+
+    this.searchSubscription = this.searchQueryChanged
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((value) => {
+        this.search = value;
+        this.getSearchPlan(
+          this.NumberOfPage,
+          this.NumberOfLimit,
+          this.search,
+          this.sortBy,
+          this.sortOrder
+        );
+      });
   }
-  getPlans(pageNumber:number, limit:number, search:string) {
+
+  onSearchInput() {
+    this.searchQueryChanged.next(this.searchQuery);
+  }
+
+  ngOnDestroy(): void {
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
+  }
+
+  // Fetch plans data
+  getPlans(
+    pageNumber: number,
+    limit: number,
+    search: string,
+    sortBy: 'externalName' | 'createdOn',
+    sortOrder: 'asc' | 'desc'
+  ) {
     this.global.showLoader();
     this.plans
-      .getPlans(pageNumber, limit, search)
+      .getPlans(pageNumber, limit, search, sortBy, sortOrder)
       .pipe(takeUntil(this.global.componentDestroyed(this)))
       .subscribe((res) => {
         if (res) {
-          this.plansData = res.data;
-          this.planAllData = this.plansData;
+          this.planAllData = res.data;
           this.plan = this.planAllData.plans;
+
+          console.log(this.plan);
           this.planLength = this.planAllData.totalCount;
+          this.plansSearchData = this.planAllData.plans;
           this.global.hideLoader();
+
+          // Calculate pagination details
+          this.totalNumberOfPlan = this.planAllData.totalCount;
+          this.totalPages = Math.ceil(this.totalNumberOfPlan / limit);
+          this.hasNextPage = pageNumber < this.totalPages;
+          if (search.length > 0) {
+            this.totalNumberOfPlanBySearch = this.planAllData.totalCount;
+            this.plansearchDataNextPage =
+              this.totalNumberOfPlanBySearch <= limit;
+          } else {
+            this.plansearchDataNextPage = false;
+          }
         } else {
           this.global.hideLoader();
         }
       });
   }
 
-  /** Announce the change in sort state for assistive technology. */
+  // Handle sort change
   announceSortChange(sortState: Sort) {
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
+    this.sortBy = sortState.active as 'externalName' | 'createdOn';
+    this.sortOrder = sortState.direction as 'asc' | 'desc';
+    this.getPlans(
+      this.pageNumber,
+      this.limit,
+      this.search,
+      this.sortBy,
+      this.sortOrder
+    );
   }
 
-  viewPlanById(id:string) {
+  // Handle view plan action
+  viewPlanById(id: string) {
     this.router.navigate([`/plans/create/${id}`]);
   }
 
+  // Handle plan deletion
   onDelete(id: string) {
     this.plans.deletePlan(id).subscribe({
       next: (res) => {
@@ -95,9 +174,9 @@ export class PlansListingComponent {
         });
       },
     });
-    this.getPlans(this.pageNumber, this.limit, this.search);
   }
 
+  // Handle delete success
   deleteSuccess(id: string) {
     const dialogRef = this.dialog.open(CouponsDeleteSuccessComponent, {
       width: '422px',
@@ -107,19 +186,21 @@ export class PlansListingComponent {
         deleteId: id,
       },
     });
-    this.plans
-      .getPlans(this.pageNumber, this.limit, this.search)
-      .subscribe((data) => {
-        this.planLength = data['plans'].length;
-      });
     dialogRef.afterClosed().subscribe(() => {
       if (this.planLength === 0 && this.pageNumber > 1) {
         this.onPrevious();
       }
-      this.getPlans(this.pageNumber, this.limit, this.search);
+      this.getPlans(
+        this.pageNumber,
+        this.limit,
+        this.search,
+        this.sortBy,
+        this.sortOrder
+      );
     });
   }
 
+  // Handle delete confirmation dialog
   openDelete(id: string) {
     this.dialogRef = this.dialog.open(DeleteConfirmationComponent, {
       panelClass: 'dialog-curved',
@@ -136,25 +217,73 @@ export class PlansListingComponent {
     });
   }
 
+  // Pagination - Previous page
   onPrevious() {
     if (this.pageNumber > 1) {
       this.pageNumber--;
-      this.getPlans(this.pageNumber, this.limit, this.search);
+      this.getPlans(
+        this.pageNumber,
+        this.limit,
+        this.search,
+        this.sortBy,
+        this.sortOrder
+      );
     }
   }
 
+  // Pagination - Next page
   onNext() {
     this.pageNumber++;
-    this.getPlans(this.pageNumber, this.limit, this.search);
+    this.getPlans(
+      this.pageNumber,
+      this.limit,
+      this.search,
+      this.sortBy,
+      this.sortOrder
+    );
   }
 
+  // Set number of entries per page
   setLimit(event: any) {
     this.limit = event.value;
-    this.getPlans(this.pageNumber, this.limit, this.search);
+    this.getPlans(
+      this.pageNumber,
+      this.limit,
+      this.search,
+      this.sortBy,
+      this.sortOrder
+    );
   }
 
-  searchItem(event: any) {
-    const search = event.target.value;
-    this.getPlans(this.pageNumber, this.limit, search);
+  // Fetch search plan
+  getSearchPlan(
+    pageNumber: number,
+    limit: number,
+    search: string,
+    sortBy: 'externalName' | 'createdOn',
+    sortOrder: 'asc' | 'desc'
+  ) {
+    this.global.showLoader();
+    this.plans
+      .getPlans(pageNumber, limit, search, sortBy, sortOrder)
+      .pipe(takeUntil(this.global.componentDestroyed(this)))
+      .subscribe((data) => {
+        if (data) {
+          this.planAllData = data;
+          this.plan = data.data.plans;
+          this.plansSearchData = data.data.plans;
+          this.totalNumberOfPlan = data.data.totalCount;
+          this.totalPages = Math.ceil(this.totalNumberOfPlan / this.limit);
+          this.hasNextPage = this.pageNumber < this.totalPages;
+          this.plansearchDataNextPage = this.totalNumberOfPlan <= this.limit;
+          this.global.hideLoader();
+          if (
+            this.totalNumberOfPlan > this.allPlansData ||
+            this.totalNumberOfPlan == 0
+          ) {
+            this.allPlansData = this.totalNumberOfPlan;
+          }
+        }
+      });
   }
 }
